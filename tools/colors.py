1ba3a081c837f928b33d9f650688eeaaecd85d64
grenui92@gmail.com <grenui92@gmail.com>
2023-09-16 23:16:31 +0300

    create class to save colors tuples

---
+++ tools/colors.py
@@ -0,0 +1,8 @@
+class Colors:
+    light_green = (0, 1, 0.5, 1)
+    red = (1, 0, 0, 1)
+    blue = (0, 0, 1, 1)
+    pink = (1, 0, 1, 1)
+    black = (0, 0, 0, 1)
+    gray = (0.1, 0.1, 0.1, 0.1)
+    for_buttons = (0, 1, 0.5, 1)
